f68381b8844adc79505397dddd6db71b2bb47bed
Malphaet <max.rigaut@gmail.com>
2013-07-19 18:31:50 +0200

    Implemented Danbooru website

--- websites/danbooru.py
+++ websites/danbooru.py
@@ -17,7 +17,7 @@
 
 
 t_base_website="http://danbooru.donmai.us"
-t_website=t_base_website+"/posts"
+t_website=t_base_website+"/posts?"
 message="Have fun"
 
 import safebooru as model
@@ -26,7 +26,8 @@ model.t_website=t_website
 
 t_tag=model.t_tag
 t_npage="&page="
-post_per_page=20
+post_per_page=19
+delete_link=model.delete_link
 
 #delete_link=model.delete_link
 def build_request(tags):
@@ -36,10 +37,24 @@ def build_request(tags):
 	for t in tags[1:]:	adress+='+'+t
 	return adress
 
-make_link=model.make_link
+def make_link(link):
+	return t_base_website+link
 
 def get_page_number(page,nb):
 	if nb==0: return page
 	return page+t_npage+str(nb)
 
-find_next_picture=model.find_next_picture
+def find_next_picture(page,max_image):
+	for art in page.find_all("article"):
+		link=art.a
+		try:
+			lk=link.img
+			if lk:
+				pict=lk.get('src')
+				lk=link.get("href")
+				nb=int((lk.split('?')[0]).split('/')[-1])
+				yield nb,lk,t_base_website+'/'+pict
+		except GeneratorExit:	
+			return
+		except:
+			pass
